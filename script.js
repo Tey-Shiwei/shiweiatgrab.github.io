01230c916ab8be675431a0df732e197f8642dc65
shiwei.tey <shiwei.tey@grabtaxi.com>
2024-04-03 02:12:11 +0800

    V3

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ fetchMessages().then(data => {
 }).catch(error => {
     console.error('JSON read Error:', error);
     message_dict = {
-        "paragraph1": "To all:<br>First off, I want to express my deepest and sincere gratitude to each and every one of you. The excitement was indescribable when I received a job offer from Grab after numerous rounds (>5) of interviews. Accepting this opporunity has been one of the proudest moments in my life. The continuous support and friendship I've experienced here have been amazing, made me enjoying every moment of my time at Grab.",
+        "paragraph1": "---<br>First off, I want to express my deepest and sincere gratitude to each and every one of you. The excitement was indescribable when I received a job offer from Grab after numerous rounds (>5) of interviews. Accepting this opporunity has been one of the proudest moments in my life. The continuous support and friendship I've experienced here have been amazing, made me enjoying every moment of my time at Grab.",
         "paragraph2": "I appreciate the exciting opportunities given during my time here. I was first welcomed with the warmth of broken pipelines, Azure migration, AWS migration and OVO ingestions along with many orphaned datasets ownership. Within a few short months, my hiring manager moved on, followed shortly by my new manager and colleagues. This left me as the only remaining GFG ADW member. However, my current manager, an expert in the data field was able to turn these challenges into opportunities, guiding me and the team to rebuild the entire ADW platform into what we have today. A special thanks goes to the Caspian team for their unwavering support during this period of transition and platform setup.",
-        "paragraph3": "I want to let you all know how much I'm going to miss working with you. Our time together wasn't just about work, it was an exciting journey. Leaving behind the friendships and connections we've built makes this decision incredibly tough. Each and every one of you has made this experience truly memorable for me. While I am excited about the new chapter awaiting me, it is bittersweet to say goodbye to a place that has meant so much to me. Please know that I leave with a heavy heart, but with pride in all we've achieved together.",
+        "paragraph3": "I want to let you all know how much I'm going to miss working with you. Our time together wasn't just about work, it was an exciting journey. Leaving behind the friendships and connections we've built makes this decision incredibly tough. Each and every one of you has made this experience truly memorable for me. While I am excited about the new chapter awaiting me, it is bittersweet to say goodbye to a place that has meant so much to me. Please know that I leave with a heavy heart, but with pride in all we've achieved together.<br><br>Shiwei",
         "827eaabb74575219c42595c8f9ab7dd5ef88934196cac9ed99ab807230aaac97": "Input your Grab's slack handle to view the goodbye messages.",
         "0aa751fdcd16ae43566956897f95e0ff75d3b4021d15e6bba97e350980b37ff0":"x", // Test
     }
@@ -73,7 +73,7 @@ async function displayMessages() {
         message_dict['paragraph3']
     ];
 
-    message += `<p class='typing-effect'>Last hello ${capitalName(name)}!</p>`;
+    message += `<p class='typing-effect'>Goodbye ${capitalName(name)}!</p>`;
     message += `<p class='message'>${message_dict[hashedValue]}</p>`;
 
     if (name !== 'shiwei.tey') {
@@ -123,7 +123,7 @@ backButton.addEventListener('click', function () {
 });
 
 window.onload = function () {
-    console.log('v2')
+    console.log('v3')
     inputtext.classList.add('shake');
     var images = document.querySelectorAll('.background-grid img');
     images.forEach((img) => {
