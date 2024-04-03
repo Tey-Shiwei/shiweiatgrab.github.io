4f816d8a2cf2feaf126d165e72bc346dd86c0406
shiwei.tey <shiwei.tey@grabtaxi.com>
2024-04-04 04:03:55 +0800

    Convert to mobile version

--- script.js
+++ script.js
@@ -11,17 +11,14 @@ fetchMessages().then(data => {
     message_dict = data;
 }).catch(error => {
     console.error('JSON read Error:', error);
-    message_dict = {
-        "paragraph1": "---<br>First off, I want to express my deepest and sincere gratitude to each and every one of you. The excitement was indescribable when I received a job offer from Grab after numerous rounds (>5) of interviews. Accepting this opporunity has been one of the proudest moments in my life. The continuous support and friendship I've experienced here have been amazing, made me enjoying every moment of my time at Grab.",
-        "paragraph2": "I appreciate the exciting opportunities given during my time here. I was first welcomed with the warmth of broken pipelines, Azure migration, AWS migration and OVO ingestions along with many orphaned datasets ownership. Within a few short months, my hiring manager moved on, followed shortly by my new manager and colleagues. This left me as the only remaining GFG ADW member. However, my current manager, an expert in the data field was able to turn these challenges into opportunities, guiding me and the team to rebuild the entire ADW platform into what we have today. A special thanks goes to the Caspian team for their unwavering support during this period of transition and platform setup.",
-        "paragraph3": "I want to let you all know how much I'm going to miss working with you. Our time together wasn't just about work, it was an exciting journey. Leaving behind the friendships and connections we've built makes this decision incredibly tough. Each and every one of you has made this experience truly memorable for me. While I am excited about the new chapter awaiting me, it is bittersweet to say goodbye to a place that has meant so much to me. Please know that I leave with a heavy heart, but with pride in all we've achieved together.<br><br>Shiwei",
+    message_dict = { // For local testing
         "827eaabb74575219c42595c8f9ab7dd5ef88934196cac9ed99ab807230aaac97": "Input your Grab's slack handle to view the goodbye messages.",
-        "0aa751fdcd16ae43566956897f95e0ff75d3b4021d15e6bba97e350980b37ff0":"x", // Test
+        "0aa751fdcd16ae43566956897f95e0ff75d3b4021d15e6bba97e350980b37ff0": "Hey, though we didn't work much together but thanks for your time catching up with me. You're always the go-to person for any Digibank stuffs and you'll be the future leader of Digibank in the near future!",
     }
 });
 
 // Buttons
-const stopShaking = document.getElementById('stopShaking');
+// const stopShaking = document.getElementById('stopShaking');
 const revealButton = document.getElementById('revealButton');
 const backButton = document.getElementById('backButton');
 
@@ -67,32 +64,31 @@ async function displayMessages() {
     const name = clean(nameInput.value);
     const hashedValue = await hashString(name);
     let message = '';
-    const defaultMessages = [
-        message_dict['paragraph1'],
-        message_dict['paragraph2'],
-        message_dict['paragraph3']
-    ];
+    // const defaultMessages = [
+    //     message_dict['paragraph1'],
+    //     message_dict['paragraph2'],
+    //     message_dict['paragraph3']
+    // ];
 
-    message += `<p class='typing-effect'>Goodbye ${capitalName(name)}!</p>`;
+    // if (name !== 'shiwei.tey') {
+    //     defaultMessages.forEach(paragraph => {
+    //         message += `<p class='message'>${paragraph}</p>`;
+    //     });
+    // } else {
+    //     message += `<p class='message'>Thanks for reading this!</p>`;
+    // }
+
+    message += `<p class='typing-effect'>Good bye ${capitalName(name)}!</p>`;
     message += `<p class='message'>${message_dict[hashedValue]}</p>`;
-
-    if (name !== 'shiwei.tey') {
-        defaultMessages.forEach(paragraph => {
-            message += `<p class='message'>${paragraph}</p>`;
-        });
-    } else {
-        message += `<p class='message'>Thanks for reading this!</p>`;
-    }
-
     messageParagraph.innerHTML = message;
 }
 
 // Events
-stopShaking.addEventListener('click', function () {
-    inputtext.classList.remove('shake');
-    this.textContent = "I'll shake for you!";
-    this.classList.add('shake', 'clicked');
-});
+// stopShaking.addEventListener('click', function () {
+//     inputtext.classList.remove('shake');
+//     this.textContent = "I'll shake!";
+//     this.classList.add('clicked');
+// });
 
 revealButton.addEventListener('click', async function () {
     // Reveal
@@ -111,11 +107,14 @@ revealButton.addEventListener('click', async function () {
     }
     // Hide
     inputtext.innerHTML = 'Point the pictures ' + '<i class="fas fa-hand-point-right"></i>';
-    inputtext.classList.remove('shake')
-    stopShaking.classList.add('hidden');
+    inputtext.style.marginTop = "5px";
+    inputtext.style.marginBottom = "5px";
+    container.style.pointerEvents = 'none';
+    // inputtext.classList.remove('shake')
+    // stopShaking.classList.add('hidden');
     // Changes
-    heading.textContent = 'Missing you guys in Grab!';
-    document.body.style.backgroundColor = '#C7F6C7';
+    heading.textContent = 'Missing you guys!';
+    document.body.style.backgroundColor = '#fff';
 });
 
 backButton.addEventListener('click', function () {
@@ -124,7 +123,7 @@ backButton.addEventListener('click', function () {
 
 window.onload = function () {
     console.log('v3')
-    inputtext.classList.add('shake');
+    // inputtext.classList.add('shake');
     var images = document.querySelectorAll('.background-grid img');
     images.forEach((img) => {
         var rotation = Math.floor(Math.random() * 91) - 45;
